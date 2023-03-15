f1de061aeaffb211ecc3a8a8ae32991bf022bd73
lotad384 <carloscpd@outlook.es>
2023-03-15 16:37:41 +0100

    prueba1

--- Controllers/TInformesController.cs
+++ Controllers/TInformesController.cs
@@ -89,6 +89,7 @@ namespace gestionDiversidad.Controllers
         {
             TInforme informe = buscarInforme(nifAlumno, nifMedico, fecha);
             MemoryStream stream = new MemoryStream(informe.Contenido);
+            //HOla esto es un comentario
             return new FileStreamResult(stream, "application/pdf");
             
 
