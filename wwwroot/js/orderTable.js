f2972186bf3fa18ef67e7d6e14840ef30e41befa
lotad384 <carloscpd@outlook.es>
2023-05-06 14:35:15 +0200

    TAuditoria --> Cambio css en la tabla
    1) Se está cambiando el estilo de las tablas para que esté mejor. El siguiente paso es que salga de quién es la lista.

--- wwwroot/js/orderTable.js
+++ wwwroot/js/orderTable.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    $('#example').DataTable({
+    $('#listaAuditorias').DataTable({
         "language": {
             "search": "Buscar:",
             "lengthMenu": "Mostrar _MENU_ entradas",
@@ -13,6 +13,23 @@ document.addEventListener("DOMContentLoaded", function () {
             },
             "zeroRecords": "No se encontraron resultados",
             "emptyTable": "No hay datos disponibles en la tabla",
+            "infoEmpty": "Mostrando 0 a 0 de 0 entradas"
+        }
+    });
+    $('#listaInformes').DataTable({
+        "language": {
+            "search": "Buscar:",
+            "lengthMenu": "Mostrar _MENU_ entradas",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ entradas)",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ entradas",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            },
+            "zeroRecords": "No se encontraron resultados",
+            "emptyTable": "No hay informes disponibles en esta tabla",
             "infoEmpty": "Mostrando 0 a 0 de 0 entradas"
         }
     });
