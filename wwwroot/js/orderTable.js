99bf6fa8226fd321866c2d6f793e98c1ed200c08
lotad384 <carloscpd@outlook.es>
2023-05-07 13:37:02 +0200

    TPRofesores --> Mejora tabla css
    1) Lo mismo, pero por alguna razón, ahora va incluso docencia.

--- wwwroot/js/orderTable.js
+++ wwwroot/js/orderTable.js
@@ -101,4 +101,21 @@ document.addEventListener("DOMContentLoaded", function () {
             "infoEmpty": "Mostrando 0 a 0 de 0 entradas"
         }
     });
+    $('#listaProfesores').DataTable({
+        "language": {
+            "search": "Buscar:",
+            "lengthMenu": "Mostrar _MENU_ profesores",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ entradas)",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ entradas",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            },
+            "zeroRecords": "No se encontraron resultados",
+            "emptyTable": "No hay profesores disponibles en esta tabla",
+            "infoEmpty": "Mostrando 0 a 0 de 0 entradas"
+        }
+    });
 });
