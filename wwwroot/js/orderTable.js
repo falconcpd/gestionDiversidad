1c6a6e2737c2dd7c3ab7a81b23ef9275a080ac93
lotad384 <carloscpd@outlook.es>
2023-05-07 13:19:16 +0200

    TDocencias: Mejora css de la tabla
    1) Se ha mejorado el css de la tabla, aunque por alguna extraña razón como en los médicos, no va.  Se barajará la posibilidad de usar otros esquemas de tablas.

--- wwwroot/js/orderTable.js
+++ wwwroot/js/orderTable.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 "previous": "Anterior"
             },
             "zeroRecords": "No se encontraron resultados",
-            "emptyTable": "No hay datos disponibles en la tabla",
+            "emptyTable": "No hay auditorías disponibles en la tabla",
             "infoEmpty": "Mostrando 0 a 0 de 0 entradas"
         }
     });
@@ -81,6 +81,23 @@ document.addEventListener("DOMContentLoaded", function () {
             },
             "zeroRecords": "No se encontraron resultados",
             "emptyTable": "No hay médicos disponibles en esta tabla",
+            "infoEmpty": "Mostrando 0 a 0 de 0 entradas"
+        }
+    });
+    $('#listaDocencias').DataTable({
+        "language": {
+            "search": "Buscar:",
+            "lengthMenu": "Mostrar _MENU_ entradas",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ entradas)",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ entradas",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            },
+            "zeroRecords": "No se encontraron resultados",
+            "emptyTable": "No hay docencias disponibles en esta tabla",
             "infoEmpty": "Mostrando 0 a 0 de 0 entradas"
         }
     });
