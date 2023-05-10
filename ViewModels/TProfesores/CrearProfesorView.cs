ba0186cf97fc3b8e230e6362c67142f3f723b3d7
lotad384 <carloscpd@outlook.es>
2023-05-10 14:57:21 +0200

    TProfesores --> Mejora del css en general
    1) Se ha mejorado el css de todos los ficheros que tengan que ver con profesor en general.

--- ViewModels/TProfesores/CrearProfesorView.cs
+++ ViewModels/TProfesores/CrearProfesorView.cs
@@ -6,22 +6,22 @@ namespace gestionDiversidad.ViewModels.TProfesores
 
     public class CrearProfesorView
     {
-        [Required(ErrorMessage = "El Nif no puede estar vacío.")]
+        [Required(ErrorMessage = "El Nif no puede estar vacío")]
         [Remote(action: "verificarNif", controller: "TUsuarios", ErrorMessage = "El NIF ya está en uso")]
         public string? Nif { get; set; }
-        [Required(ErrorMessage = "El Usuario no puede estar vacío.")]
+        [Required(ErrorMessage = "El Usuario no puede estar vacío")]
         [Remote(action: "verificarCrearNombreUsuario", controller: "TUsuarios", ErrorMessage = "El nombre del usuario ya está en uso")]
         public string? Usuario { get; set; }
-        [Required(ErrorMessage = "La contraseña no puede estar vacía.")]
+        [Required(ErrorMessage = "La contraseña no puede estar vacía")]
         public string? Password { get; set; }
-        [Required(ErrorMessage = "La confirmación de la contraseña no puede estar vacía.")]
-        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
+        [Required(ErrorMessage = "La confirmación de la contraseña no puede estar vacía")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden")]
         public string? ConfirmPassword { get; set; }
-        [Required(ErrorMessage = "El nombre no puede estar vacío.")]
+        [Required(ErrorMessage = "El nombre no puede estar vacío")]
         public string? Nombre { get; set; }
-        [Required(ErrorMessage = "El primer apellido no puede estar vacío.")]
+        [Required(ErrorMessage = "El primer apellido no puede estar vacío")]
         public string? Apellido1 { get; set; }
-        [Required(ErrorMessage = "El segundo apellido no puede estar vacío.")]
+        [Required(ErrorMessage = "El segundo apellido no puede estar vacío")]
         public string? Apellido2 { get; set; }
     }
 }
